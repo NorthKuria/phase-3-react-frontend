99913024013cfe799aa8a7ec49510cd0ecb7f8c7
NorthKuria <kuri4martin@gmail.com>
2022-09-08 16:45:40 +0300

    flesh out login page

--- gumzo-chat/src/pages/Home.js
+++ gumzo-chat/src/pages/Home.js
@@ -5,10 +5,10 @@ function Home () {
     return (
         <div>
             <div className="hero flex max-w-7xl mx-auto items-center">
-                <img src={require('./chat_icon.png')} alt="chat icon" width="100" height="100" />
+                <img src={require('../chat_icon.png')} alt="chat icon" width="100" height="100" />
                 <h2>Say something</h2>
             </div>   
-            <div className="flex max-w-7xl">
+            <div className="flex max-w-7xl mx-auto">
                 <ChatBox />
                 <ChatMenu />
             </div> 
