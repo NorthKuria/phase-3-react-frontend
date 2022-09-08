4d56af14eb23549e39af8d9c2112ea243c9bb525
NorthKuria <kuri4martin@gmail.com>
2022-09-08 20:55:10 +0300

    fetch and display messages

--- gumzo-chat/src/components/ChatBox.js
+++ gumzo-chat/src/components/ChatBox.js
@@ -1,7 +1,10 @@
-function ChatBox () {
+function ChatBox ({messages}) {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto">
             <p>Chat box</p>
+            {messages.map((message) => (
+                <p key={message.id}>{message.body}</p>
+            ))}
         </div>
     )
 }
--- gumzo-chat/src/pages/Home.js
+++ gumzo-chat/src/pages/Home.js
@@ -1,7 +1,17 @@
+import { useEffect, useState, React } from 'react';
+
 import ChatBox from '../components/ChatBox';
 import ChatMenu from '../components/ChatMenu';
 
 function Home () {
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:9292/messages')
+        .then(r => r.json())
+        .then(data => setMessages(data))
+      }, []);
+
     return (
         <div>
             <div className="hero flex max-w-7xl mx-auto items-center">
@@ -9,7 +19,7 @@ function Home () {
                 <h2>Say something</h2>
             </div>   
             <div className="flex max-w-7xl mx-auto">
-                <ChatBox />
+                <ChatBox messages={messages} />
                 <ChatMenu />
             </div> 
       </div>
